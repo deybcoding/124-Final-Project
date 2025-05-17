f2acfa442de8289bb5d5263d91c969f1c86bca25
Dave Laurence <davelaurnz@gmail.com>
2025-05-17 14:00:43 +0800

    feat: implemt constants and evaluator

---
+++ constants.py
@@ -0,0 +1,10 @@
+# ANSI color codes for terminal output
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+RESET = '\033[0m'
+
+# Configuration
+VERBOSE_OUTPUT = True  # Print type information
+COLOR_OUTPUT = True    # Use colored output
